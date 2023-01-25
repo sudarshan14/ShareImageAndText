4d0ba73946bdf1bd611f7b52fb21183584cdfab6
sudarashan_bhatt_d4 <sudarashan.bhatt@d4insight.com>
2023-01-25 17:24:25 +0530

    initial commit

---
+++ app/src/main/java/com/amlavati/shareimageandtext/MainActivity.java
@@ -0,0 +1,68 @@
+package com.amlavati.shareimageandtext;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView imageView = findViewById(R.id.imageView);
+        findViewById(R.id.share).setOnClickListener(v -> {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) imageView.getDrawable();
+            Bitmap bitmap = bitmapDrawable.getBitmap();
+            shareImageandText(bitmap);
+        });
+    }
+
+
+    private void shareImageandText(Bitmap bitmap) {
+        Uri uri = getmageToShare(bitmap);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+
+        // putting uri of image to be shared
+        intent.putExtra(Intent.EXTRA_STREAM, uri);
+
+        // adding text to share
+        intent.putExtra(Intent.EXTRA_TEXT, "Sharing Image");
+
+        // Add subject Here
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Subject Here");
+
+        // setting type to image
+        intent.setType("image/png");
+
+        // calling startactivity() to share
+        startActivity(Intent.createChooser(intent, "Share Via"));
+    }
+    private Uri getmageToShare(Bitmap bitmap) {
+        File imagefolder = new File(getCacheDir(), "images");
+        Uri uri = null;
+        try {
+            imagefolder.mkdirs();
+            File file = new File(imagefolder, "shared_image.png");
+            FileOutputStream outputStream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 90, outputStream);
+            outputStream.flush();
+            outputStream.close();
+            uri = FileProvider.getUriForFile(this, "com.anni.shareimage.fileprovider", file);
+        } catch (Exception e) {
+            Toast.makeText(this, "" + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+        return uri;
+    }
+}
---
+++ app/src/main/java/com/amlavati/shareimageandtext/MainActivityKotlin.kt
@@ -0,0 +1,62 @@
+package com.amlavati.shareimageandtext
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.FileOutputStream
+
+class MainActivityKotlin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_kotlin)
+
+        val imageView = findViewById<ImageView>(R.id.imageViewKotlin)
+
+        findViewById<Button>(R.id.share).setOnClickListener {
+            val bitmapDrawable = imageView.drawable as BitmapDrawable
+            val bitmap = bitmapDrawable.bitmap
+            shareImageandText(bitmap)
+        }
+    }
+
+
+    private fun shareImageandText(bitmap: Bitmap) {
+        val uri = getImageToShare(bitmap)
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.putExtra(Intent.EXTRA_STREAM, uri)
+        intent.putExtra(Intent.EXTRA_SUBJECT, "subject")
+        intent.putExtra(Intent.EXTRA_TEXT, "I am sharing image")
+        intent.type = "image/png"
+        startActivity(Intent.createChooser(intent, "share via"))
+
+    }
+
+
+    private fun getImageToShare(bitmap: Bitmap): Uri? {
+
+        val imageFolder = File(cacheDir, "images")
+        var uri: Uri? = null
+        try {
+            imageFolder.mkdirs()
+            val file = File(imageFolder, "share_image.png")
+            val outputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.PNG, 90, outputStream)
+            outputStream.flush()
+            outputStream.close()
+
+            uri = FileProvider.getUriForFile(this, "com.anni.shareimage.fileprovider", file)
+        } catch (e: Exception) {
+            Toast.makeText(this, "" + e.message, Toast.LENGTH_LONG).show()
+
+        }
+        return uri
+    }
+}
